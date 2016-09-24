48cfa1ce91078fe17620d5912bba1ca1c7c1e0d4
ladislavlisy <ladislav.lisy@seznam.cz>
2016-09-24 22:09:53 +0200

    Add NUnit Test

---
+++ PaycheckitasTest/Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using PaycheckitasLib;
+
+namespace PaycheckitasTest
+{
+	[TestFixture ()]
+	public class TestPayrollData
+	{
+		[Test ()]
+		public void TestWorkingHoursShouldBe_Zero_At_Start ()
+		{
+			PayrollData data = new PayrollData ();
+			Assert.AreEqual (0m, data.WorkedHours ());
+		}
+	}
+}
